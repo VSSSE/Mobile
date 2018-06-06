1c48e57963247ef7cc9bf863c0c99c834df29233
Anastasia Schwed <nshmyreva@gmail.com>
2018-06-06 22:07:26 +0200

    Add Google policy for first few files PART 2

--- myapplication/src/main/java/org/dhbw/se/movietunes/controller/SearchSimilarSongsController.java
+++ myapplication/src/main/java/org/dhbw/se/movietunes/controller/SearchSimilarSongsController.java
@@ -1,37 +1,23 @@
 package org.dhbw.se.movietunes.controller;
 
-import android.content.Context;
+import java.util.List;
 import org.dhbw.se.movietunes.http.SpotifyCommunication;
 import org.dhbw.se.movietunes.model.Song;
-import java.util.List;
 
 /**
  * Created by anastasia.schwed on 11/26/2017.
  */
 
 public class SearchSimilarSongsController {
-    Context appContext;
-    SpotifyCommunication spotifyCommunication;
+  private SpotifyCommunication spotifyCommunication;
 
-    public SearchSimilarSongsController(Context appContext) {
-        this.appContext = appContext;
-        spotifyCommunication = new SpotifyCommunication();
-    }
+  public SearchSimilarSongsController() {
+    spotifyCommunication = new SpotifyCommunication();
+  }
 
-
-    public List<Song> findSimilarSongs(String trackId) {
-
-//       FAKE:
-//     List<Song> result = new ArrayList<>();
-//        for (int i = 0; i < 5; i++) {
-//            Song song = new Song();
-//            song.setSongTitle("similar to " + input + " " + i);
-//            result.add(song);
-//        }
-//
-        List<Song> result = spotifyCommunication.getRecommendations(trackId);
-        return result;
-    }
+  public List<Song> findSimilarSongs(String trackId) {
+    return spotifyCommunication.getRecommendations(trackId);
+  }
 
 }
 
