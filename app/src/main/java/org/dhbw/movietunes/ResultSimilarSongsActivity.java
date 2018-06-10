d70c9a2fbbd3a7b5f346f9ee7e1eb91b65767b0f
c0d3d3v <live.for.program@gmail.com>
2018-06-10 12:35:22 +0200

    reformating and process bar

--- app/src/main/java/org/dhbw/movietunes/ResultSimilarSongsActivity.java
+++ app/src/main/java/org/dhbw/movietunes/ResultSimilarSongsActivity.java
@@ -1,10 +1,16 @@
 package org.dhbw.movietunes;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.PopupMenu;
 import java.util.ArrayList;
 import java.util.List;
 import org.dhbw.movietunes.controller.SearchSimilarSongsController;
@@ -13,9 +19,9 @@ import org.dhbw.movietunes.controller.SearchSimilarSongsController;
  * Created by anastasia.schwed on 11/21/2017.
  */
 
-public class ResultSimilarSongsActivity extends AppCompatActivity {
+public class ResultSimilarSongsActivity extends AppCompatActivity  {
 
-  private List<String> similars;
+
 
   protected void onCreate(Bundle savedInstanceState) {
 
@@ -24,14 +30,14 @@ public class ResultSimilarSongsActivity extends AppCompatActivity {
     setContentView(R.layout.result_similar_songs_activity);
 
     Intent intent = getIntent();
+    ListView similarList = findViewById(R.id.similarSongs_list_view);
+
 
     String trackId = intent.getStringExtra("TRACK_ID");
     SearchSimilarSongsController controller
             = new SearchSimilarSongsController(this);
 
     controller.execute(trackId);
-
-    similars = new ArrayList<>();
 
   }
 
@@ -42,12 +48,5 @@ public class ResultSimilarSongsActivity extends AppCompatActivity {
     return true;
   }
 
-  public List<String> getSimilars() {
-    return similars;
-  }
-
-  public void setSimilars(List<String> similars) {
-    this.similars = similars;
-  }
 
 }
