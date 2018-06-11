27db351adf992843aa3836bf39d15ba90ebcb32d
c0d3d3v <live.for.program@gmail.com>
2018-06-11 19:07:43 +0200

    finish movie Adapter

--- app/src/main/java/org/dhbw/movietunes/controller/SearchMovieTitleController.java
+++ app/src/main/java/org/dhbw/movietunes/controller/SearchMovieTitleController.java
@@ -1,9 +1,13 @@
 package org.dhbw.movietunes.controller;
 
+import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
+import org.dhbw.movietunes.R;
 import org.dhbw.movietunes.ResultMovieTitleActivity;
 import org.dhbw.movietunes.http.MovieCommunication;
+import org.dhbw.movietunes.list.MovieAdapter;
+import org.dhbw.movietunes.model.Movie;
 
 /**
  * Created by anastasia.schwed on 11/26/2017.
@@ -24,10 +28,9 @@ public class SearchMovieTitleController extends AsyncSearchController {
 
   @Override
   protected void postResult(List<Object> result) {
-
-    /*MovieAdapter adapter = new MovieAdapter(activity, (ArrayList<Movie>)(List)(result));
-    ListView list = activity.findViewById(R.id.movie_list_view);
-    list.setAdapter(adapter);*/
+    MovieAdapter adapter = new MovieAdapter(activity, (ArrayList<Movie>)(List)(result));
+    ListView list = activity.findViewById(R.id.movietitles_list_view);
+    list.setAdapter(adapter);
   }
 
 }
