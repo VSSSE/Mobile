e79a261271a78cf4855bf806e0f037bc37b93ae7
c0d3d3v <live.for.program@gmail.com>
2018-06-11 18:36:35 +0200

    add movie list item

--- app/src/main/java/org/dhbw/movietunes/ResultMovieTitleActivity.java
+++ app/src/main/java/org/dhbw/movietunes/ResultMovieTitleActivity.java
@@ -3,6 +3,8 @@ package org.dhbw.movietunes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+import org.dhbw.movietunes.controller.SearchMovieSoundtracksController;
+import org.dhbw.movietunes.controller.SearchMovieTitleController;
 
 public class ResultMovieTitleActivity extends MainMenu {
 
@@ -15,9 +17,13 @@ public class ResultMovieTitleActivity extends MainMenu {
     setContentView(R.layout.result_movie_titles_activity);
 
     Intent intent = getIntent();
-    String songTitle = intent.getStringExtra(EXTRA_MESSAGE);
     TextView song = findViewById(R.id.song);
+
+    String songTitle = intent.getStringExtra(EXTRA_MESSAGE);
     song.setText(songTitle);
+
+    SearchMovieTitleController controller = new SearchMovieTitleController(this);
+    controller.execute(songTitle);
   }
 
 }
