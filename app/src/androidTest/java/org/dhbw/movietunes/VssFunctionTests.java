33fea66d6375bef8caae7d2b3284ee3a7341e41e
c0d3d3v <live.for.program@gmail.com>
2018-06-19 18:25:41 +0200

    first up

--- app/src/androidTest/java/org/dhbw/movietunes/VssFunctionTests.java
+++ app/src/androidTest/java/org/dhbw/movietunes/VssFunctionTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MainActivityEspressoTest.class, ResultSountracksTest.class})
-public class VssApplicationTests {
+@SuiteClasses({MainActivityEspressoTest.class, ResultSountracksTest.class})
+public class VssFunctionTests {
 
 }
